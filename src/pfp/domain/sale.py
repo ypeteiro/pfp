28cb133cefe80d9b7fb1e12f0ae5858d183ce5d3
ypeteiro <ypeteiro@gmail.com>
2026-08-11 23:35:00 +0200

    feat: add persisted sale domain

---
+++ src/pfp/domain/sale.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
+
+@dataclass(frozen=True, slots=True)
+class Sale:
+    datetime: datetime
+    symbol: str
+    shares: Decimal
+    amount: Decimal
+    price: Decimal
+    broker: str = "Trade Republic"
